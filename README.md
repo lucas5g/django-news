5ed8284aabdf4516a7ff03c680dcef3baec30dbb
lucas5g <lucasdesousa19@hotmail.com>
2026-09-09 16:31:13 -0300

    Add article creation and editing functionality; update templates and URL routing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # django-news
+
+## Duvidas 
+1 - Por que após a edição é redirecionado para a tela de detalhe do artigo.
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import  ListView, DetailView 
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 
@@ -20,3 +20,12 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body')
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
